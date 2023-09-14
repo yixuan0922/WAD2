70e9603e536e838bc3f7e3d13d144a5306c041e7
yixuan0922 <yixuan0922@hotmail.com>
2023-09-14 16:45:39 +0800

    Fix: fixed routing of the entire web. Yet to add Homepage. disable prettier

---
+++ schedoo/src/main.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+
+createApp(App).use(store).use(router).mount("#app");
+import "bootstrap/dist/js/bootstrap.js";
+
---
+++ schedoo/src/views/LandingView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="LandingView">
+    <NavHeader/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import NavHeader from "@/components/NavHeader.vue";
+
+export default {
+  name: "LandingView",
+  components: {
+    // HelloWorld,
+    NavHeader,
+  },
+};
+</script>
