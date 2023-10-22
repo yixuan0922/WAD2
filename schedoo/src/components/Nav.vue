50436fddd6f495a7d35b53c931ee4c67ea66e929
Ngee Tian <warkionw12@gmail.com>
2023-10-22 11:24:44 +0800

    transferred profile to nav

--- schedoo/src/components/Nav.vue
+++ schedoo/src/components/Nav.vue
@@ -10,6 +10,41 @@ style="background-color: #e8da8b;"
 >
     <div class="container">
         <a class="navbar-brand" id="navBarLogo" href="#"><img src="@/assets/navschedoo.png" width="125"></a>
+
+        <div class="navbar-text me-auto align-bottom justify-content-end" id="wholeProfile">
+            <!-- <li class="nav-item"> -->
+            <!-- <RouterLink class="nav-link text-white" to="#"> Home </RouterLink> -->
+            <!-- <router-link class="nav-link" :to="{name: 'Login'}">Login</router-link>
+            </li> -->
+            <div class="profile pButton" ref="profile">
+            <span @click="toggle">{{this.$store.state.profileInitials}}</span>
+                <div class="profile-menu" id="profileMenu" hidden>
+                <div class="info">
+                    <p class="initials">{{this.$store.state.profileInitials}}</p>
+                    <div class="right">
+                    <p>{{this.$store.state.profileFirstName}} {{this.$store.state.profileLastName}}</p>
+                    <p>{{this.$store.state.profileUsername}} </p>
+                    <p>{{this.$store.state.profileEmail}} </p>
+                    </div>
+                </div>
+                <div class="options">
+                    <div class="option">
+                    <router-link class-="option" to="#">
+                    <userIcon class="icon"/>
+                    <p>Profile</p>
+                    </router-link>
+                    </div>
+                    <div class="option">
+                    <router-link class-="option" to="#">
+                    <!-- <userIcon class="icon"/> -->
+                    <p>Sign Out</p>
+                    </router-link>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+
         <button class="navbar-toggler" type="button" 
         data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
         aria-controls="navbarSupportedContent" aria-expanded="false" 
@@ -42,12 +77,15 @@ style="background-color: #e8da8b;"
                 </ul>
             </li>
         </ul>
-        <span class="navbar-text">
+        <span class="navbar-text" style="right: 5%">
             <router-link class="nav-link" :to="{name: 'Login'}">Login</router-link>
         </span>
-        <!-- </div> -->
-    <!-- </div>
-</nav> -->
+        
+
+        
+        </div>
+    </div>
+</nav>
 
 
 
@@ -77,11 +115,58 @@ style="background-color: #e8da8b;"
                 </a>
             </li>
             </ul> -->
-        </div>
+        <!-- </div>
         
-        </div>
-    </nav>
+    </div>
+</nav> -->
 </template>
+
+<script>
+export default {
+  name: "header",
+  data() {
+    return {
+      hidden: true,
+      loggedin: false,
+    }
+  },
+  // ready: function () {
+  //   checkAuth();
+  //   this.profileBtn();
+  // },
+  // computed: {
+  //   profileBtn() {
+  //     document.getElementById("wholeProfile").hidden = !this.loggedin;
+  //   }
+  // },
+  methods: {
+    toggle() {
+      this.hidden = !this.hidden;
+      document.getElementById("profileMenu").hidden = this.hidden;
+    },
+    // signOut() {
+    //   const auth = getAuth();
+
+    //   signOut(auth).then(() => {
+    //     alert ("Successful Signout");
+    //     this.loggedin = false;
+    //   }).catch((error) => {
+    //     alert ("Failed Signout");
+    //   });
+    // },
+    // checkAuth() {
+    //   var user = firebase.auth().currentUser;
+
+    //   if (user) {
+    //     this.loggedin = true;
+    //   } else {
+    //     this.loggedin = false;
+    //   }
+    // }
+  }
+};
+
+</script>
 
 <style scoped>
 .navbar {
@@ -95,5 +180,71 @@ style="background-color: #e8da8b;"
     font-size: large;
     font-family: system-ui;
     letter-spacing: 0.05em;
+}
+
+.profile {
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  /* right: 2%;
+  top: 10px; */
+  margin-left: 20px;
+  z-index: 1;
+  font-size: large;
+  border-radius: 50%;
+  color: #ffff;
+  background-color: #303030;
+
+  .profile-menu {
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width:260px;
+    background-color: #e3cf62;
+    color: black !important;
+    font-weight: bold;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    border-radius: 5%;
+
+    .info {
+      display: flex;
+      align-items: center;
+      padding: 15px;
+      border-bottom: 1px solid #fff;
+
+      .initials {
+        position: initial;
+        width: 40px;
+        height: 40px;
+        background-color: #303030;
+        color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        font-weight: normal;
+      }
+
+      .right {
+        flex: 1;
+        margin-left: 24px;
+        text-decoration: none;
+
+        p:nth-child(1) {
+          font-size: 14px;
+        }
+
+        p:nth-child(2), 
+        p:nth-child(3) {
+          font-size: 12px;
+        }
+      }
+
+    }
+  }
 }
 </style>
