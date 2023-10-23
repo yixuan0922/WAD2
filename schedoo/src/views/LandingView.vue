fd2071301c3cab7dbda3422268cc06fd563ca07f
Ngee Tian <warkionw12@gmail.com>
2023-10-23 17:25:29 +0800

    changed to load tester

--- schedoo/src/views/LandingView.vue
+++ schedoo/src/views/LandingView.vue
@@ -8,7 +8,7 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-import Header from "@/components/Header.vue";
+import Header from "@/components/HeaderTester.vue";
 import Nav from "@/components/Nav.vue";
 
 export default {
