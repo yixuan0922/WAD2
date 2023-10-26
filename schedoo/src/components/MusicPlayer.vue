27af40be23f7f0dc625b2dbe58bbeff2fb2e556d
Ngee Tian <warkionw12@gmail.com>
2023-10-26 10:00:29 +0800

    added url input, trying to add title display

--- schedoo/src/components/MusicPlayer.vue
+++ schedoo/src/components/MusicPlayer.vue
@@ -1,71 +1,124 @@
 <template>
-  <div>
-    <div id="youtube-player"
-    style="width: 0; height: 0"></div>
-    <button @click="playVideo">Play</button>
-    <button @click="pauseVideo">Pause</button>
-    <button @click="stopVideo">Stop</button>
     <div>
-        <input type="text" :value="src" size=50>
+        <h1>{{ videoTitle }}</h1>
+      <div id="youtube-player"></div>
+      <div>
+        <input v-model="videoUrl" type="text" placeholder="Enter YouTube video URL" />
+        <button @click="loadVideo" class="btn btn-primary">Load Video</button>
+        <button @click="playVideo" class="btn btn-success">Play</button>
+        <button @click="pauseVideo" class="btn btn-warning">Pause</button>
+        <button @click="stopVideo" class="btn btn-danger">Stop</button>
+      </div>
     </div>
-  </div>
-</template>
+  </template>
+  
+  <script>
 
-<script>
-export default {
-  data() {
-    return {
-      player: null,
-      src: "https://www.youtube.com/watch?v=ULQhvIGG27Q",
-    };
-  },
-  methods: {
-    playVideo() {
-      if (this.player) {
-        this.player.playVideo();
-      }
+  export default {
+    data() {
+      return {
+        player: null,
+        videoUrl: '', // Added video URL data property
+        videoTitle: '', // Added video title data property
+        apiKey: process.env.VUE_APP_YOUTUBE_API_KEY,
+      };
     },
-    pauseVideo() {
-      if (this.player) {
-        this.player.pauseVideo();
-      }
-    },
-    stopVideo() {
-      if (this.player) {
-        this.player.stopVideo();
-      }
-    },
-    onYouTubeIframeAPIReady() {
+    methods: {
+      playVideo() {
+        if (this.player) {
+          this.player.playVideo();
+        }
+      },
+      pauseVideo() {
+        if (this.player) {
+          this.player.pauseVideo();
+        }
+      },
+      stopVideo() {
+        if (this.player) {
+          this.player.stopVideo();
+        }
+      },
+      loadVideo() {
+        // Validate the YouTube URL
+        const videoId = this.extractVideoId(this.videoUrl);
+  
+        if (videoId) {
+          if (this.player) {
+            this.player.loadVideoById(videoId);
+          } else {
+            this.createYouTubePlayer(videoId);
+          }
+        } else {
+          alert('Invalid YouTube video URL.');
+        }
+      },
+      extractVideoId(url) {
+        // Extract the video ID from the YouTube URL
+        const videoIdMatch = url.match(/[?&]v=([a-zA-Z0-9_-]+)/);
+        if (videoIdMatch) {
+          return videoIdMatch[1];
+        }
+        return null;
+      },
+            // Fetch video title using YouTube Data API
+            fetchVideoTitle(videoId) {
+        const apiKey = process.env.YOUTUBE_API_KEY; // Replace with your own API key
+        console.log(apiKey);
+        const apiUrl = `https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&id=${videoId}&part=snippet`;
+        
+        fetch(apiUrl)
+            .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+            })
+            .then(data => {
+            if (data.items && data.items.length > 0) {
+                const video = data.items[0];
+                this.videoTitle = video.snippet.title;
+            } else {
+                console.error('Video not found or data unavailable.');
+            }
+            })
+            .catch(error => {
+            console.error('Error fetching video title:', error);
+            });
+        },
+
+      createYouTubePlayer(videoId) {
         // eslint-disable-next-line
-      this.player = new YT.Player('youtube-player', {
-        videoId: 'ULQhvIGG27Q', // Replace with your YouTube video ID
-        playerVars: {
-          controls: 0,
-          showinfo: 0,
-          rel: 0,
+        this.player = new YT.Player('youtube-player', {
+            videoId: videoId,
+            playerVars: {
+            controls: 0,
+            showinfo: 0,
+            rel: 0,
+            },
+            events: {
+            onReady: (event) => {
+                this.player = event.target;
+                // Fetch video title and set it
+                this.videoTitle = this.fetchVideoTitle(videoId);
+            },
+            },
+        });
         },
-        events: {
-          onReady: (event) => {
-            this.player = event.target;
-          },
-        },
-      });
+      onYouTubeIframeAPIReady() {
+        // Load YouTube Iframe Player API script
+        const tag = document.createElement('script');
+        tag.src = 'https://www.youtube.com/iframe_api';
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  
+        // Expose onYouTubeIframeAPIReady to the window
+        window.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady;
+      },
     },
-  },
-  mounted() {
-    // Load YouTube Iframe Player API script
-    console.log(this.src.substring(this.src.indexOf("=")+1));
-    const tag = document.createElement('script');
-    tag.src = "ULQhvIGG27Q"
-    
-    const firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    
-    // Ensure the script is loaded before calling onYouTubeIframeAPIReady
-    tag.onload = () => {
-      window.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady;
-    };
-  },
-};
-</script>
-
+    mounted() {
+      this.onYouTubeIframeAPIReady();
+    },
+  };
+  </script>
+  
