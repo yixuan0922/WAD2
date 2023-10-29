8409b0d4fc6026ac740f93512abef158afcdbaaf
Ngee Tian <warkionw12@gmail.com>
2023-10-29 15:00:05 +0800

    added nav to settings

--- schedoo/src/views/SettingsView.vue
+++ schedoo/src/views/SettingsView.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="SettingsView">
+      <Nav/>
       <Settings/>
     </div>
   </template>
@@ -7,12 +8,14 @@
   <script>
   // @ is an alias to /src
   // import HelloWorld from "@/components/HelloWorld.vue";
+  import Nav from "@/components/Nav.vue"
   import Settings from "@/components/Settings.vue";
   
   export default {
     name: "SettingsView",
     components: {
       // HelloWorld,
+      Nav,
       Settings,
     },
   };
