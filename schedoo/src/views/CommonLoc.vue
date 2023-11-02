0f2d69f1cbc9aea51eb80150c74d7f4ce7cfcb71
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2023-11-02 22:23:52 +0800

    reformatting screen... wip </3

--- schedoo/src/views/CommonLoc.vue
+++ schedoo/src/views/CommonLoc.vue
@@ -2,36 +2,58 @@
   <div id="app">
     <div class="outer-container">
       <div class="inner-container">
-        <div class="col-4 side-bar">
-          <h2 class="header">Recommended Locations:</h2>
-          <div v-for="place in placesList" :key="place.name">
-            <div
-              class="row loc"
-              @click="
-                recenterMap(
-                  place.geometry.location,
-                  place.name,
-                  place.vicinity,
-                  map
-                );
-                selectPlace(place);
-              "
-            >
-              <div class="col-3 loc-img" id="img" v-if="checkPhoto(place)">
-                <img
-                  :src="imageSource"
-                />
-              </div>
-              <div class="col loc-details">
-                <p class="loc-name">{{ place.name }}</p>
-                <p class="loc-address">{{ place.vicinity }}</p>
-              </div>
-            </div>
-          </div>
-          <button @click="setLocation(selectedPlace)">Set Location</button>
+        <div class="col-6 side-bar">
+           
+           <div class="card" style="width: 100%;"> 
+               <img src="https://api-private.atlassian.com/users/5f116c2970b8a90025c6efa9/avatar?initials=public" class="card-img-top" alt="..."> 
+               <div class="card-body"> 
+                   <h5 class="card-title">Card title</h5> 
+                   <p class="card-text">Some quick example text to make up the bulk of the card's content.</p>  
+               </div> 
+               <div class="card-body"> 
+                   <a href="#" class="card-link">Card link</a> 
+                   <a href="#" class="card-link">Another link</a> 
+               </div> 
+           </div> 
+           <!-- BS card: End --> 
+          
         </div>
 
-        <div class="col" id="map"></div>
+        <div class="col right-side" style="display: block; margin-right: 40px">
+          <div class="row" id="map" style="margin-bottom: 5px"></div>
+          <div class="row">
+            <h2 class="header">Recommended Locations:</h2>
+          </div>
+          <div class="row">
+            <div
+              v-for="place in placesList"
+              :key="place.name"
+              style="padding-left: 2px"
+            >
+              <div
+                class="row loc"
+                @click="
+                  recenterMap(
+                    place.geometry.location,
+                    place.name,
+                    place.vicinity,
+                    map
+                  );
+                  selectPlace(place);
+                "
+              >
+                <div class="col-3 loc-img" v-if="checkPhoto(place)">
+                  <img id="img" :src="imageSource" />
+                </div>
+                <div class="col loc-details">
+                  <p class="loc-name">{{ place.name }}</p>
+                  <p class="loc-address">{{ place.vicinity }}</p>
+                </div>
+              </div>
+            </div>
+            <button @click="setLocation(selectedPlace)">Set Location</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -45,7 +67,7 @@ import { onMounted, ref } from "vue";
 let placesList = ref([]);
 let map = ref("");
 let selectedPlace = {};
-let imageSource = ''
+let imageSource = "";
 
 const loader = new Loader({
   apiKey: process.env.VUE_APP_GOOGLE_API_KEY,
@@ -150,7 +172,7 @@ function checkPhoto(place) {
     });
   }
 
-  return imageSource
+  return imageSource;
 }
 </script>
 
@@ -162,10 +184,10 @@ body {
 #map {
   border: transparent;
   border-radius: 20px;
-  background-color: lightgray;
-  width: 500px;
-  height: 90vh;
-  margin: 0px 40px;
+  background-color: pink;
+  width: 100%;
+  height: 400px;
+  margin: auto;
   object-fit: contain;
   display: flex;
   position: sticky !important;
@@ -180,12 +202,12 @@ body {
   text-align: left;
   font-weight: bold;
   margin-top: 0px;
-  padding-top: 25px !important;
+  padding-top: 400px !important;
   padding-bottom: 10px !important;
   color: rgb(55, 51, 51);
   margin-bottom: 20px;
-  position: fixed;
-  top: 0px;
+  position: fixed !important;
+  top: 448px !important;
   background-color: white;
   width: 100%;
   padding-bottom: 3px;
@@ -208,33 +230,39 @@ body {
 .loc-img {
   border: transparent;
   border-radius: 20px;
-  background-color: transparent !important;
+  background-color: pink !important;
   width: 100px !important;
   height: 100px !important;
-  margin-bottom: 10px;
+  /* margin-bottom: 10px; */
   object-fit: contain;
   overflow: hidden;
-  padding: 0 !important;
+  padding-top: 8px !important;
 }
 
-img {
+#img {
   display: block;
   width: 100%;
   height: 100%;
 }
 
 .row.loc {
-  margin: 5px;
+  margin: 10px !important;
 }
 
 .loc-details {
   text-align: left;
-  margin-left: 3px;
+  margin-left: 15px !important;
+  display: block !important;
+  padding: 0px;
 }
 
 .loc-name {
   font-weight: bold;
   font-size: 20px;
   margin-bottom: 2px;
+}
+
+.loc-address {
+  margin-bottom: 0px !important;
 }
 </style>
