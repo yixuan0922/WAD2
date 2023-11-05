a336098cb7d74e7f0bebef4df3f7b9e6b36936de
Ngee Tian <warkionw12@gmail.com>
2023-11-05 12:02:01 +0800

    standardised modal style

--- schedoo/src/components/SavedModal.vue
+++ schedoo/src/components/SavedModal.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="saved-modal" v-if="show">
       <div class="modal-content">
+        <div class="modal-header">
+          <h2 style="color:green">Saved!</h2>
+          <span @click="closeModal" class="close-button">&times;</span>
+        </div>
         <div class="modal-body">
-          <p>Your changes have been saved successfully!</p>
+          <p >Your changes have been saved successfully!</p>
         </div>
         <div class="modal-footer">
-          <button @click="closeModal">OK</button>
+          <button class="btn btn-primary" @click="closeModal">OK</button>
         </div>
       </div>
     </div>
@@ -32,7 +36,7 @@
   <style scoped>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
 
-  .saved-modal {
+  /* .saved-modal {
     font-family: "poppins", sans-serif;
     position: fixed;
     top: 0;
@@ -45,15 +49,30 @@
     align-items: center;
     z-index: 9999;
     border-radius: 13px;
-  }
+  } */
   
   .modal-content {
-    background-color: green;
+    /* background-color: #fff;
     color: white;
     border-radius: 10px;
     width: 80%;
     max-width: 400px;
     height: 100px;
+    display: none;  */
+    font-family: "poppins", sans-serif;
+
+    position: fixed; 
+    top: 50%; 
+    left: 50%; 
+    border-radius: 10px; 
+    transform: translate(-50%, -50%); 
+    padding: 20px; 
+    width: 450px; 
+    background: #e9fce9; 
+    border: 1px solid #ccc; 
+    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2); 
+    z-index: 999;
+    color: black;
   }
   
   .modal-body {
@@ -66,14 +85,28 @@
     justify-content: flex-end;
     padding: 10px;
   }
+
+  .close-button {
+    cursor: pointer;
+  }
   
   button {
-    background-color: white;
+    /* background-color: white;
     color: green;
     border: none;
     border-radius: 5px;
     padding: 5px 10px;
     cursor: pointer;
-    transform: translateY(-40px);
+    transform: translateY(-40px); */
+    float: right; 
+    margin-right: 0px; 
+    margin-top:3px;
+  }
+
+  .modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
   }
   </style>
