c38d33d097c00b8c71127ebd42cbc7dbfcd10974
Ngee Tian <warkionw12@gmail.com>
2023-11-05 11:47:26 +0800

    font poppins

--- schedoo/src/components/SavedModal.vue
+++ schedoo/src/components/SavedModal.vue
@@ -33,6 +33,7 @@
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
 
   .saved-modal {
+    font-family: "poppins", sans-serif;
     position: fixed;
     top: 0;
     left: 0;
