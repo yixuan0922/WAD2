dbb8c2931ec3a4b8ff1882bbc0bf9d07db40b753
Ngee Tian <warkionw12@gmail.com>
2023-11-07 02:05:16 +0800

    finalise looks format w/ data

--- schedoo/src/components/InviteCard.vue
+++ schedoo/src/components/InviteCard.vue
@@ -7,34 +7,72 @@
         />
       </div>
       <div class="text col">
-        <h6 class="row">Event name</h6>
+        <h6 class="row title">
+          {{ event.title }}
+        </h6>
+        <h1 class="row email">
+          {{ event.invitorEmail }}
+        </h1>
         <p class="card-text row">
-          Sat Oct 28 2023 08:00:00 to Sun Oct 29 2023 08:00:00
+          {{ eventStart }} to {{ eventEnd }}
         </p>
       </div>
     </div>
     <hr class="cardDivider" />
     <div class="card-body action">
-      <button>Accept</button>
-      <button class="decline">Decline</button>
+      <button @click="acceptInvite(event)">Accept</button>
+      <button class="decline" @click="declineInvite(event)">Decline</button>
     </div>
   </div>
 </template>
-<script></script>
 
-<style>
+<script>
+
+
+export default {
+  props: {
+    event: {
+      type: Object,
+    },
+  },
+  computed: {
+    eventStart() {
+      // Use JavaScript to extract the desired part of the string
+      return this.event.start.split(" GMT")[0];
+    },
+    eventEnd() {
+      // Use JavaScript to extract the desired part of the string
+      return this.event.start.split(" GMT")[0];
+    }
+  },
+  methods: {
+    acceptInvite() {
+      this.$emit("accept-invite", this.event); // Emit an event to notify the parent component
+    },
+    declineInvite() {
+      this.$emit("decline-invite", this.event); // Emit an event to notify the parent component
+    },
+  },
+};
+
+</script>
+
+<style scoped>
 .card {
   width: 300px;
   margin: auto;
+  padding-bottom: 15px;
   margin-bottom: 8px;
+  padding: 0 !important;
 }
 
 .img img {
-  width: 100%;
+  width: 30px;
+  height: 30px;
   justify-content: center !important;
 }
 
-img .container .col-2 {
+.img.container.col-2 {
   display: flex;
   padding-right: 5px;
 }
@@ -49,15 +87,27 @@ img .container .col-2 {
   text-align: left;
 }
 
-h6 {
+.title, .email {
+  position: absolute;
   font-size: 18px;
   margin-bottom: 2px;
   font-weight: bold !important;
+  color: black;
+}
+
+.title {
+  transform: translateY(-20px);
+
+}
+
+.email {
+  font-size: 12px;
 }
 
 .card-text {
   font-size: 14px;
   color: gray;
+  transform: translateY(30px);
 }
 
 .cardDivider {
@@ -82,5 +132,12 @@ h6 {
   background-color: whitesmoke;
   border: 2px solid #2c3e50;
   color: #2c3e50;
+}
+
+@media (max-width:1000px) {
+  .card {
+    width: 250px;
+    transform: translateX(-25px);
+  }
 }
 </style>
