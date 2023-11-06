ffe3216cbefcc7fb896e35f92c45c1b4909191b8
Ngee Tian <warkionw12@gmail.com>
2023-11-07 04:24:21 +0800

    CSS styling

--- schedoo/src/components/AcceptInvite.vue
+++ schedoo/src/components/AcceptInvite.vue
@@ -1,27 +1,31 @@
 <template>
-    <div>
+    <div class="main">
       <fieldset>
-        <legend>Event details</legend>
-        <b>Title:</b>  {{ event.title }} <br/>
+        <legend>{{ event.title }}</legend>
+        <!-- <b>Title:</b>  {{ event.title }} <br/> -->
         <b>Invited By:</b> {{ event.invitorEmail }} <br/>
         <b>Start:</b>  {{ event.start }} <br/>
         <b>End:</b>  {{ event.end }} <br/>
         <b>ID:</b>  {{ event.id }} <br/>
-        <b>Invitees</b>
-        <ul>
-          <li v-for="(invitee, index) in event.invitees" :key="index">{{invitee.email + "(" + invitee.status + ")" + invitee.location}}</li>
-        </ul><br/>
+        <div class="invitees">
+          <b>Invitees</b>
+          <ul>
+            <li v-for="(invitee, index) in event.invitees" :key="index">{{invitee.email + "(" + invitee.status + ")" + invitee.location}}</li>
+          </ul><br/>
+        </div>
       </fieldset>
       <fieldset>
-          <legend>Provide your location</legend>
+          <!-- <legend>Provide your location</legend> -->
           <div class="container">
-            <div class='row'> 
-              Location: <input type="text" v-model="location">
+            <div class='row location'> 
+              Provide your Location: <input type="text" v-model="location">
             </div>
           </div>
-          <button class='buttonSave' @click="load">Load</button>
-          <button class='buttonSave' @click="this.$emit('close-modal')">Close</button>
-          <!-- <button class='buttonSave' @click="updateEvent">Confirm</button> -->
+          <div class="btns mt-5 mb-5">
+            <button class='buttonSave btn btn-success' @click="load">Load</button>
+            <button class='buttonSave btn btn-danger' @click="this.$emit('close-modal')">Close</button>
+            <!-- <button class='buttonSave' @click="updateEvent">Confirm</button> -->
+          </div>
       </fieldset>
     </div>
   </template>
@@ -80,6 +84,7 @@
           //     // })
           //     this.$emit('close-modal');
           // },
+
           load () {
             // console.log(this.location);
             const currUserId = this.$store.state.profileId;
@@ -120,51 +125,66 @@
             this.$emit('close-modal');
           },
       },
-    props: {
-      text: String,
-      event: Object
-    },mounted() {
-        this.id = this.event.id;
-        this.title = this.event.title;
-        this.start = formatDate(this.event.start);
-        this.end = formatDate(this.event.end);
-        this.startTime = formatTime(this.event.start);
-        this.endTime = formatTime(this.event.end);
-        this.allDay = Boolean(this.event.allDay);
-        this.invitees = this.event.invitees;
+      props: {
+    text: String,
+    event: Object
+  },
+  mounted() {
+    this.id = this.event.id;
+    this.title = this.event.title;
+    this.start = formatDate(this.event.start);
+    this.end = formatDate(this.event.end);
+    this.startTime = formatTime(this.event.start);
+    this.endTime = formatTime(this.event.end);
+    this.allDay = Boolean(this.event.allDay);
+    this.invitees = this.event.invitees;
 
-        this.invitorId = this.event.invitorId;
-        this.invitorEmail = this.event.invitorEmail;
+    this.invitorId = this.event.invitorId;
+    this.invitorEmail = this.event.invitorEmail;
 
-        this.category = this.event.category;
-        console.log(this.category);
-    }
-  };
-  
-  function formatDate(date) {
-      // YYYY-MM-DD 
+    this.category = this.event.category;
+    console.log(this.category);
+  }
+};
+
+function formatDate(date) {
+  // YYYY-MM-DD 
       var options = {year: "numeric", month: '2-digit', day: "2-digit"};
       let string = date.toLocaleDateString("ko-KR", options).replace(/\. /g, "-")
   
       return string.substr(0, string.length - 1)
-  }
+}
 
-  function formatTime(date) {
-     // HH:MM
+function formatTime(date) {
+  // HH:MM
     let hours = date.getHours();
     let minutes = date.getMinutes();
 
-    hours = hours < 10 ? '0' + hours : hours;
+  hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
 
-    let time = `${hours}:${minutes}`;
+  let time = `${hours}:${minutes}`;
 
     return time
-  }
+}
   </script>
   
   <style>
   .buttonSave {
     margin-left: 10px
+  }
+  .modal .modal-inner[data-v-4935b9f2] {
+    border-radius: 5%;
+    padding-bottom: 0px !important;
+    padding-top: 30px !important;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+    background-color: beige;
+  }
+  .location {
+    width: 80%;
+    transform: translateX(15%);
+  }
+  .invitees {
+    margin-top: 20px;
   }
   </style>
